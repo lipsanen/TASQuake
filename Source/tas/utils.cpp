df8b0982c52ca80abac2cac73ee45f6947695252
Jukspa <jukspaz@gmail.com>
2019-11-27 19:07:20 +0200

    Optimized strafing algorithm to use continuous fractions to calculate the best fmove/smove.

--- Source/tas/utils.cpp
+++ Source/tas/utils.cpp
@@ -1,6 +1,8 @@
 #include "cpp_quakedef.hpp"
 #include "utils.hpp"
 #include <cmath>
+#include <cstdint>
+#include <algorithm>
 
 bool IsZero(double number)
 {
@@ -23,63 +25,78 @@ float AngleModDeg(float deg)
 	return number * (360.0 / 65536);
 }
 
+// Algorithm from http://esrg.sourceforge.net/docs/paper_brap_reduced.pdf
+void BrapAlg(double& outInteger1, double& outInteger2, int64_t max_int)
+{
+	double ratio = outInteger1 / outInteger2;
+	int64_t p[3], q[3];
+	int64_t remainder = 1e18;
+	int64_t divisor = std::round(remainder * ratio);
+	p[0] = 1;
+	p[1] = 0;
+	p[2] = 0;
+	q[0] = 0;
+	q[1] = 0;
+	q[2] = 0;
+
+
+	for (int i = 0; remainder != 0 && q[0] <= max_int; ++i)
+	{
+		p[2] = p[1];
+		p[1] = p[0];
+		q[2] = q[1];
+		q[1] = q[0];
+
+		int64_t dividend = divisor;
+		divisor = remainder;
+		int64_t a = dividend / divisor;
+		remainder = dividend % divisor;
+		if (i == 0)
+		{
+			p[0] = a;
+			q[0] = 1;
+		}
+		else
+		{
+			p[0] = a * p[1] + p[2];
+			q[0] = a * q[1] + q[2];
+		}
+
+	}
+
+	if (remainder == 0 && q[0] <= max_int)
+	{
+		int mult = max_int / std::fmax(q[0], p[0]);
+		outInteger1 = p[0] * mult;
+		outInteger2 = q[0] * mult;
+	}
+	else
+	{
+		int mult = max_int / std::fmax(q[1], p[1]);
+		outInteger1 = p[1] * mult;
+		outInteger2 = q[1] * mult;
+	}
+}
+
 void ApproximateRatioWithIntegers(double& number1, double& number2, int max_int)
 {
+	double ratio = number1 / number2;
 	int num1Sign = number1 >= 0 ? 1 : -1;
 	int num2Sign = number2 >= 0 ? 1 : -1;
 	number1 = std::abs(number1);
 	number2 = std::abs(number2);
 
-	int min_int = max_int / 2;
-	double little, big;
 	bool flip = number1 > number2;
-	if (flip)
-	{
-		little = number2;
-		big = number1;
-	}
-	else if (number1 == number2)
-	{
-		return;
-	}
-	else
-	{
-		little = number1;
-		big = number2;
-	}
-
-	double ratio = little / big;
-	double min_error = 1;
-	double newLittle = little;
-	double newBig = big;
+	if(flip)
+		std::swap(number1, number2);
 
 
-	for (int a = max_int; a >= min_int; --a)
-	{
-		int b = std::round(a * ratio);
-		double error = std::abs((static_cast<double>(b)) / a - ratio);
-
-		if (error < min_error)
-		{
-			min_error = error;
-			newLittle = b;
-			newBig = a;
-
-			if (min_error == 0)
-				break;
-		}
-
-	}
+	BrapAlg(number1, number2, max_int);
 
 	if (flip)
-	{
-		number1 = newBig * num1Sign;
-		number2 = newLittle * num2Sign;
-	}
-	else
-	{
-		number2 = newBig * num2Sign;
-		number1 = newLittle * num1Sign;
-	}
+		std::swap(number1, number2);
+
+	number1 *= num1Sign;
+	number2 *= num2Sign;
 
 }
