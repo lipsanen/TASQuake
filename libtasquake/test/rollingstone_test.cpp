9d96b1236981f850b31efe56fe2f81a757aee855
lipsanen <jukspaz@gmail.com>
2023-01-06 02:19:58 +0200

    Minor changes to the benchmark

--- libtasquake/test/rollingstone_test.cpp
+++ libtasquake/test/rollingstone_test.cpp
@@ -14,29 +14,29 @@ TEST_CASE("Stone rolls up slope")
 
 static void CornerFunc(TASQuake::Player* player) {
     TASQuake::MemorylessSim(player);
-    if(player->m_vecPos.x <= 10.0) {
-        player->m_vecPos.y = std::max(player->m_vecPos.y, 0.0f);
+    if(player->m_vecPos.x < 10.0) {
+        player->m_vecPos.y = std::min(player->m_vecPos.y, 0.0f);
     }
 }
 TEST_CASE("Optimizer bench") {
-    PlaybackInfo info;
-    FrameBlock block1;
-    block1.convars["tas_strafe_yaw"] = 0.0;
-    block1.convars["tas_strafe"] = 1.0;
+    BENCHMARK_ADVANCED("Corner bench")(Catch::Benchmark::Chronometer meter) {
+        PlaybackInfo info;
+        FrameBlock block1;
+        block1.convars["tas_strafe_yaw"] = 0.0;
+        block1.convars["tas_strafe"] = 1.0;
 
-    FrameBlock block2;
-    block2.convars["tas_strafe_yaw"] = 90.0;
-    block2.frame = 100;
+        FrameBlock block2;
+        block2.convars["tas_strafe_yaw"] = 90.0;
+        block2.frame = 100;
 
-    info.current_script.blocks.push_back(block1);
-    info.current_script.blocks.push_back(block2);
+        info.current_script.blocks.push_back(block1);
+        info.current_script.blocks.push_back(block2);
 
-    TASQuake::OptimizerSettings settings;
-    settings.m_vecAlgorithms.push_back(std::shared_ptr<TASQuake::OptimizerAlgorithm>(new TASQuake::FrameBlockMover()));
-    double optimal = info.Get_Last_Frame() - 10.0; // This should be the y coordinate
-    TASQuake::BenchTest(CornerFunc, &settings, &info, optimal);
+        TASQuake::OptimizerSettings settings;
+        settings.m_uResetToBestIterations = 1;
+        settings.m_vecAlgorithms.push_back(std::shared_ptr<TASQuake::OptimizerAlgorithm>(new TASQuake::FrameBlockMover()));
+        double optimal = 126; // This should be the y coordinate
 
-    BENCHMARK("Corner bench") {
-        TASQuake::BenchTest(CornerFunc, &settings, &info, optimal);
+        meter.measure([&]{TASQuake::BenchTest(CornerFunc, &settings, &info, optimal);});
     };
 }
