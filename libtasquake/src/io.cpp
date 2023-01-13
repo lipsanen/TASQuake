57e0f7bbb26378b988afec7424bfe73957a0efa7
lipsanen <jukspaz@gmail.com>
2023-01-13 21:02:20 +0200

    Add some file writing stuff

---
+++ libtasquake/src/io.cpp
@@ -0,0 +1,174 @@
+#include "libtasquake/io.hpp"
+#include <cstdlib>
+#include <cstring>
+
+using namespace TASQuakeIO;
+
+Buffer::~Buffer() {
+    if(!readonly)
+        free(ptr);
+}
+
+bool Buffer::Realloc(size_t newsize) {
+    void* p = std::realloc(ptr, newsize);
+
+    if(p) {
+        this->size = newsize;
+        ptr = p;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+std::shared_ptr<Buffer> Buffer::CreateBuffer(size_t initialSize) {
+    auto ptr = std::make_shared<Buffer>();
+    if(ptr) {
+        ptr->Realloc(initialSize);
+    }
+    return ptr;
+}
+
+
+std::shared_ptr<Buffer> Buffer::CreateFromCString(const char* str) {
+    auto ptr = std::make_shared<Buffer>();
+    if(ptr) {
+        ptr->ptr = (void*)str;
+        ptr->size = strlen(str);
+        ptr->readonly = true;
+    }
+    return ptr;
+}
+
+FileReadInterface FileReadInterface::Init(const char* filepath) {
+    FileReadInterface iface;
+    iface.m_pStream.open(filepath);
+    return iface;
+}
+
+bool FileReadInterface::CanRead() {
+    return m_pStream.good() && !m_pStream.eof();
+}
+
+bool FileReadInterface::GetLine(std::string& str) {
+    std::getline(m_pStream, str);
+    return !str.empty() || CanRead();
+}
+
+std::uint32_t FileReadInterface::Read(void* dest, std::uint32_t buf_size) {
+    m_pStream.read((char*)dest, buf_size);
+    if(m_pStream.eof()) {
+        return m_pStream.gcount();
+    } else {
+        return buf_size;
+    }
+}
+
+BufferReadInterface BufferReadInterface::Init(void* buffer, std::uint32_t size) {
+    BufferReadInterface output;
+    output.m_pBuffer = buffer;
+    output.m_uSize = size;
+    return output;
+}
+
+bool BufferReadInterface::CanRead() {
+    return m_uFileOffset < m_uSize;
+}
+
+bool BufferReadInterface::GetLine(std::string& str) {
+    str.clear();
+    if(!CanRead()) {
+        return false;
+    }
+
+    while(str.empty() && m_uFileOffset < m_uSize) {
+        std::uint32_t endOffset;
+        std::uint32_t bytesLeft = m_uSize - m_uFileOffset;
+        const char* strStart = ((char*)m_pBuffer) + m_uFileOffset;
+
+        for(endOffset=0; endOffset < bytesLeft; ++endOffset) {
+            char c = strStart[endOffset];
+            if(c == '\0' || c == '\n') {
+                break;
+            }
+        }
+
+        m_uFileOffset += endOffset + 1; // Skip past the newline or null character
+
+        if(endOffset != 0) {
+            str.assign(strStart, endOffset);
+        }
+    }
+
+    return !str.empty();
+}
+
+std::uint32_t BufferReadInterface::Read(void* dest, std::uint32_t buf_size) {
+    std::uint32_t bytesLeft = m_uSize - m_uFileOffset;
+    std::uint32_t bytesToRead = std::min(bytesLeft, buf_size);
+    std::memcpy(dest, (std::uint8_t*)m_pBuffer + m_uFileOffset, bytesToRead);
+    m_uFileOffset += bytesToRead;
+    return bytesToRead;
+}
+
+FileWriteInterface FileWriteInterface::Init(const char* filepath) {
+    FileWriteInterface iface;
+    iface.m_pStream.open(filepath);
+    return iface;
+}
+
+bool FileWriteInterface::CanWrite() {
+    return m_pStream.good();
+}
+
+bool FileWriteInterface::WriteLine(const std::string& str) {
+    m_pStream << str << '\n';
+    return true;
+}
+
+std::uint32_t FileWriteInterface::Write(const void* src, std::uint32_t buf_size) {
+    m_pStream.write((const char*)src, buf_size);
+    return buf_size;
+}
+
+void FileWriteInterface::Finalize() {
+    m_pStream.close();
+}
+
+BufferWriteInterface BufferWriteInterface::Init() {
+    BufferWriteInterface iface;
+    iface.m_pBuffer = Buffer::CreateBuffer(128);
+    return iface;
+}
+
+bool BufferWriteInterface::CanWrite() {
+    return true;
+}
+
+bool BufferWriteInterface::WriteLine(const std::string& str) {
+    Write(str.c_str(), str.size());
+    char newline = '\n';
+    Write(&newline, 1);
+    return true;
+}
+
+std::uint32_t BufferWriteInterface::Write(const void* src, std::uint32_t buf_size) {
+    std::uint32_t newSize = std::max<std::uint32_t>(1, m_pBuffer->size);
+    std::uint32_t targetSize = m_uFileOffset + buf_size;
+
+    while(newSize < targetSize) {
+        newSize <<= 1;
+    }
+
+    if(newSize != m_pBuffer->size) {
+        m_pBuffer->Realloc(newSize);
+    }
+
+    memcpy((std::uint8_t*)m_pBuffer->ptr + m_uFileOffset, src, buf_size);
+    m_uFileOffset += buf_size;
+    return buf_size;
+}
+
+void BufferWriteInterface::Finalize() {
+    m_pBuffer->size = m_uFileOffset;
+}
